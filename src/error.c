888cd67a2babd2479ae7f56cf57c8a2a9e882712
Deleuze <75162059+Stonksmen@users.noreply.github.com>
2024-02-21 17:14:04 +0100

    add spec for each of our functions

--- src/error.c
+++ src/error.c
@@ -1,20 +1,33 @@
 #include "error.h"
 
-void fork_error(char *cmd) {
-    fprintf(stderr, "%s: fork failed\n", cmd);
-    return;
-}
-
+/**
+ * @fn exec_error
+ * @brief print a simple error message when an exec failed.
+ * @param cmd command name of failed command
+ * @return void
+*/
 void exec_error(char *cmd) {
     fprintf(stderr, "%s: command not found\n", cmd);
     return;
 }
 
+/**
+ * @fn open_error
+ * @brief print a simple error message when open function failed.
+ * @param file file that failed to open
+ * @return void
+*/
 void open_error(char *file) {
     fprintf(stderr, "%s: permission denied\n", file);
     return;
 }
 
+/**
+ * @fn cmd_line_error
+ * @brief print a simple error message if there was an error during the reading of the command by the readline function.
+ * @param error error raised during the reading of the command
+ * @return void
+*/
 void cmd_line_error(char *error) {
     fprintf(stderr, "error: %s\n", error);
     return;
