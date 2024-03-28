937920d98a8b0247e5c631879abd2e19338a5d14
NotRage <grousson.dylan@gmail.com>
2024-03-28 14:02:36 +0100

    ???

--- attente_1s.c
+++
@@ -1,6 +0,0 @@
-#include "src/csapp.h"
-
-int main() {
-    sleep(1);
-    return 0;
-}
--- src/global.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-#include "list.h"
-
-extern list_t bg_list;
-extern list_t fg_list;
-
-#endif // GLOBAL_H
